d7113d6058b18eea2346514b86c7394793c77cd9
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2022-11-19 15:29:36 -0500

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -40,13 +40,13 @@ class SUtil
     static public function doTheCheck()
     {
         #if android
-        if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
+        if (!grantedPermsList.contains(READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
             if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
-                AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
+                AndroidTools.requestPermissions([READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
             }  
         }
 
-        if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
+        if (!grantedPermsList.contains(READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
             if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
                 SUtil.applicationAlert("Permissions", "If you accepted the permisions for storage, good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App");
             } else {
